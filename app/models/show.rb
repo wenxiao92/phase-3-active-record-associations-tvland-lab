721069b2e947e699a448281beb1412bb915b6cf4
Wen Xiao <wennstyle@gmail.com>
2022-07-12 00:21:38 -0400

    commit tree 1d74dd1f6035936c432a30e6903ccdbd04ef0970

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.map do |full_name|
+        "#{full_name.first_name} #{full_name.last_name}" 
+    end
+  end
+
 end
